874fee9902077a6e4c499d80893d9913a17be932
Rasyid <Rasyidridla298@gmail.com>
2022-05-15 16:55:16 +0700

    create learning list feature

---
+++ Camera Simulator/ViewController/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Camera Simulator
+//
+//  Created by Rasyid Ridla on 15/05/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var learning: Learning?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("test")
+        dump(learning!)
+    }
+}
--- Camera Simulator/ViewController/LearningViewController.swift
+++ Camera Simulator/ViewController/LearningViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class LearningViewController: UIViewController {
-
+class LearningViewController: UITableViewController {
+    
+    @IBOutlet var learningTableView: UITableView!
+    
+    var listLearning: [Learning] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupTable()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension LearningViewController {
+    
+    func setupTable() {
+        let nib = UINib(nibName: "LearningTableViewCell", bundle: nil)
+        learningTableView.register(nib, forCellReuseIdentifier: "learningCell")
+        
+        listLearning.append(contentsOf: Learning.dataLearning())
+    }
+}
+
+extension LearningViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listLearning.count
     }
     
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let learningListData = listLearning[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "learningCell") as! LearningTableViewCell
+        
+        cell.learning = learningListData
+        cell.updateCell()
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let destinationVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "learningDetail") as! DetailViewController
+        destinationVC.learning = listLearning[indexPath.row]
+        
+        navigationController?.pushViewController(destinationVC, animated: true)
+    }
 }
