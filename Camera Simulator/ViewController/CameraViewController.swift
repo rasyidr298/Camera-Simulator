b29589638ed8f60bdc79dfc97e070381242c1e53
Rasyid <Rasyidridla298@gmail.com>
2022-05-18 12:32:06 +0700

    manipulate image feature

--- Camera Simulator/ViewController/CameraViewController.swift
+++ Camera Simulator/ViewController/CameraViewController.swift
@@ -6,22 +6,28 @@
 //
 
 import UIKit
+import Photos
 
 class CameraViewController: UIViewController {
     
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var objectImage: UIImageView!
+    @IBOutlet weak var afSlider: UISlider!
+    @IBOutlet weak var isoSlider: UISlider!
     
-    private var listBackgroundImage = [UIImage(named: "bg_test1"), UIImage(named: "bg_test2")]
-    private var listObjectImage = [UIImage(named: "bg_test2"), UIImage(named: "bg_test1")]
+    private var listBackgroundImage = [UIImage(named: "bg_test1"), UIImage(named: "bg_test2"), UIImage(named: "bg_test3")]
+    private var listObjectImage = [UIImage(named: "bg_test2"), UIImage(named: "bg_test1"), UIImage(named: "bg_test2")]
     private var indexImage = 0
-    var transition = CATransition()
+    private var transition = CATransition()
+    
+    private let colorFilter = CIFilter(name: "CIColorControls")!
+    private let blurFilter = CIFilter(name: "CIGaussianBlur")!
     var context = CIContext(options: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         animateImageView(type: "next")
-        blurEffect(uiImage: backgroundImage, value: 5)
+        setupView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -40,15 +46,15 @@ extension CameraViewController {
     
     func animateImageView(type: String) {
         switch type {
-            case "next" :
-                transition.type = CATransitionType.push
-                transition.subtype = CATransitionSubtype.fromRight
-            case "previous" :
-                transition.type = CATransitionType.push
-                transition.subtype = CATransitionSubtype.fromLeft
-            default:
-                transition.type = CATransitionType.push
-                transition.subtype = CATransitionSubtype.fromRight
+        case "next" :
+            transition.type = CATransitionType.push
+            transition.subtype = CATransitionSubtype.fromRight
+        case "previous" :
+            transition.type = CATransitionType.push
+            transition.subtype = CATransitionSubtype.fromLeft
+        default:
+            transition.type = CATransitionType.push
+            transition.subtype = CATransitionSubtype.fromRight
         }
         
         backgroundImage.layer.add(transition, forKey: kCATransition)
@@ -61,36 +67,91 @@ extension CameraViewController {
         CATransaction.commit()
         indexImage = indexImage < listBackgroundImage.count - 1 ? indexImage + 1 : 0
     }
-
-    func blurEffect(uiImage: UIImageView, value: Int) {
-        let currentFilter = CIFilter(name: "CIGaussianBlur")
-        let beginImage = CIImage(image: uiImage.image!)
-        currentFilter!.setValue(beginImage, forKey: kCIInputImageKey)
-        currentFilter!.setValue(value, forKey: kCIInputRadiusKey)
-
-        let cropFilter = CIFilter(name: "CICrop")
-        cropFilter!.setValue(currentFilter!.outputImage, forKey: kCIInputImageKey)
-        cropFilter!.setValue(CIVector(cgRect: beginImage!.extent), forKey: "inputRectangle")
-
-        let output = cropFilter!.outputImage
-        let cgimg = context.createCGImage(output!, from: output!.extent)
-        let processedImage = UIImage(cgImage: cgimg!)
-        uiImage.image = processedImage
+    
+    func setTriangleExpose(_ image: UIImage) -> UIImage? {
+        guard let colorControls = CIFilter(name: "CIColorControls"), let blurControls = CIFilter(name: "CIGaussianBlur") else {return nil}
+        
+        guard let cgImage = image.cgImage else {return image}
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        //brightness
+        colorControls.setValue(ciImage, forKey: "inputImage")
+        colorControls.setValue(isoSlider.value, forKey: "inputBrightness")
+        guard let imageWithFirstFilter = colorControls.value(forKey: kCIOutputImageKey) as? CIImage else { return nil }
+        
+        //blurr
+        blurControls.setValue(imageWithFirstFilter, forKey: kCIInputImageKey)
+        blurControls.setValue(afSlider.value, forKey: kCIInputRadiusKey)
+        guard let imageWithSecondFilter = blurControls.value(forKey: kCIOutputImageKey) as? CIImage else { return nil }
+        
+        let bounds = CGRect(origin: CGPoint.zero, size: image.size)
+        guard let outputCGImage = context.createCGImage(imageWithSecondFilter, from: bounds) else {return image}
+        
+        return UIImage(cgImage: outputCGImage)
+    }
+    
+    
+    private func savePhoto(){
+        PHPhotoLibrary.requestAuthorization { (status) in
+            guard status == .authorized else{
+                return
+            }
+            PHPhotoLibrary.shared().performChanges({
+                PHAssetCreationRequest.creationRequestForAsset(from: self.listBackgroundImage[self.indexImage]!)
+            }) { (success, error) in
+                if let error = error {
+                    print("Error saving photo \(error)")
+                    return
+                }
+                print("Saved photo successfully")
+            }
+        }
+    }
+    
+    private func setupView() {
+        afSlider.value = 2
+        afSlider.minimumValue = 2
+        afSlider.maximumValue = 10
+        
+        isoSlider.value = 0.0
+        isoSlider.minimumValue = 0.0
+        isoSlider.maximumValue = 0.6
+    }
+    
+    private func updateView(){
+        if let originalImage = listBackgroundImage[indexImage] {
+            backgroundImage.image = setTriangleExpose(originalImage)
+        } else {
+            backgroundImage.image = nil
+        }
     }
 }
+
 
 extension CameraViewController {
     
     @IBAction func btnNext(_ sender: Any) {
         animateImageView(type: "next")
+        setupView()
     }
     
     @IBAction func btnPrevious(_ sender: Any) {
         animateImageView(type: "previous")
+        setupView()
     }
     
     @IBAction func btnLearning(_ sender: Any) {
-            let destinationVC = UIStoryboard(name: storyBoardName, bundle: nil).instantiateViewController(withIdentifier: learningVcId) as! LearningViewController
-            navigationController?.pushViewController(destinationVC, animated: true)
+        let destinationVC = UIStoryboard(name: storyBoardName, bundle: nil).instantiateViewController(withIdentifier: learningVcId) as! LearningViewController
+        navigationController?.pushViewController(destinationVC, animated: true)
+    }
+    
+    @IBAction func afSlider(_ sender: Any) {
+        updateView()
+        print("af slider -> \(afSlider.value)")
+    }
+    
+    @IBAction func isoSlider(_ sender: Any) {
+        updateView()
+        print("iso slider -> \(isoSlider.value)")
     }
 }
